3cdaddf8cc75f365c51acdd98d04b25b0dc90d82
soloveyy <solovyov.dd@gmail.com>
2022-02-11 16:51:21 +0100

    Continue practice with testing

--- src/__tests__/movie-details.test.js
+++ src/__tests__/movie-details.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import MovieDetails from '../components/movie-details'
+import { isExportDeclaration } from 'typescript';
 
 const selectedMovie = {
     id: 1,
@@ -20,5 +21,22 @@ describe('MovieDetails component', () =>{
         const { getByText } = render(<MovieDetails movie={selectedMovie}/>)
         expect(screen.getByText(selectedMovie.title)).toBeTruthy()
         expect(screen.getByText(selectedMovie.description)).toBeTruthy()
+    })
+
+    test('should display number of ratings', ()=>{
+        const { container } = render(<MovieDetails movie={selectedMovie}/>)
+        const selected_stars = container.querySelectorAll('.orange');
+        expect(selected_stars.length).toBe(3)
+        expect(selected_stars.length).toBe(selectedMovie.avg_rating)
+    })
+
+    test('mouseover should highlight the stars', ()=>{
+        const { container } = render(<MovieDetails movie={selectedMovie}/>)
+        const stars = container.querySelectorAll('.rate-container svg');
+        stars.forEach((star,idx)=>{
+            fireEvent.mouseOver(star)
+            const highlighted_stars = container.querySelectorAll('.purple');
+            expect(highlighted_stars.length).toBe(idx+1)
+        })
     })
 })
